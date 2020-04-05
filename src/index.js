49e17d47fb8793a6566435408a7a5b197a824674
Alexej <alexejdubow@gmail.com>
2020-04-05 09:43:56 +0300

    combined two functions into one

--- src/index.js
+++ src/index.js
@@ -19,27 +19,34 @@ const bodyTheme = localStorage.getItem('theme');
 
 if (bodyTheme === 'dark-theme') {
   themeSwitch.checked = true;
-  addDarkTheme();
+  addTheme(Theme.DARK);
 } else {
   themeSwitch.checked = false;
-  addLightTheme();
+  addTheme(Theme.LIGHT);
 }
 
 themeSwitch.addEventListener('change', () => {
-  themeSwitch.checked ? addDarkTheme() : addLightTheme();
+  // themeSwitch.checked ? addDarkTheme() : addLightTheme();
+  themeSwitch.checked ? addTheme(Theme.DARK) : addTheme(Theme.LIGHT);
+
 });
 
 function buildMenu(menu) {
   const markupMenu = menu.map(item => markupTemplate(item)).join('');
   ref.menu.insertAdjacentHTML('beforeend', markupMenu);
 }
-function addLightTheme() {
+// function addLightTheme() {
+//   body.classList = '';
+//   body.classList.add(Theme.LIGHT);
+//   localStorage.setItem('theme', Theme.LIGHT);
+// }
+// function addDarkTheme() {
+//   body.classList = '';
+//   body.classList.add(Theme.DARK);
+//   localStorage.setItem('theme', Theme.DARK);
+// }
+function addTheme(colorTheme) {
   body.classList = '';
-  body.classList.add(Theme.LIGHT);
-  localStorage.setItem('theme', Theme.LIGHT);
-}
-function addDarkTheme() {
-  body.classList = '';
-  body.classList.add(Theme.DARK);
-  localStorage.setItem('theme', Theme.DARK);
+  body.classList.add(colorTheme);
+  localStorage.setItem('theme', colorTheme);
 }
